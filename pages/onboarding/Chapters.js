307e098161bbd244b4be0f3abfebc7d3b8708c71
medal17 <iyandaadeshina@gmail.com>
2022-04-22 17:12:55 +0100

    weekend commit

--- pages/onboarding/Chapters.js
+++ pages/onboarding/Chapters.js
@@ -16,13 +16,13 @@ export default function Chapters({navigation}) {
     ]
   return (
     <View style={tw`h-full`}>
-        <View style={tw`m-auto w-8/12 `}>
+        <View style={tw`m-auto w-10/12 `}>
             <Text style={tw`text-lg text-center text-green-800 font-bold `}>Welcome to Alumni Assocation of National Institute</Text>  
 
-            <View style={tw`mx-auto my-2 px-3 flex-row py-2 bg-green-800 rounded-lg`}>
+            <Pressable onPress={()=>navigation.navigate('login')} style={tw`mx-auto my-2 px-3 flex-row py-2 bg-green-800 rounded-lg`}>
                 <Text style={tw`text-white`}>Continue to National Platform  </Text>
                 <Ionicon name='md-arrow-forward-circle' style={tw`my-auto`} size={22} color='white'/>
-            </View>
+            </Pressable>
             <Text style={tw`mx-auto text-base`}>OR</Text>
             
             
