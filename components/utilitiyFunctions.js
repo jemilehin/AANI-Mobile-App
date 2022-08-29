c9761c34d7de4ac97eb63380374951b2eb6908a3
Temidayo <jemiilehin@gmail.com>
2022-08-29 02:51:57 +0100

    made textInput mapped in modal ediitable and update rendered array and member object

--- components/utilitiyFunctions.js
+++ components/utilitiyFunctions.js
@@ -1,7 +1,7 @@
 export const ConvertObjectToArray = (object) => {
     const arr = []
     for (const key in object) {
-        arr.push({name: [key], property: object[key]})
+        arr.push({name: key, property: object[key]})
     }
     return arr;
 }
